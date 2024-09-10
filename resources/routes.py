84b5ab09f163236c19c0914febc978d8c7ac6778
Lucas Lemes <lucas.heine.lemes@icloud.com>
2024-09-10 13:51:20 -0300

    Simple Doctrings added.

--- resources/routes.py
+++ resources/routes.py
@@ -12,6 +12,7 @@ blp = Blueprint("tasks", __name__, description="Operations on tasks!")
 
 @blp.route("/tasks")   #http://127.0.0.1:5000/tasks
 class ListAllTasks(MethodView):
+    ''' This Endpoint is used for show all tasks, completed or not.'''
     @blp.response(200, TaskSchema(many=True))
     def get(self):
         return TaskModel.query.all()
@@ -19,6 +20,7 @@ class ListAllTasks(MethodView):
     
 @blp.route("/uncompletedtasks")   #http://127.0.0.1:5000/uncompletetasks
 class ListUncompletedTasks(MethodView):
+    '''This class is used for show all uncompleted tasks listed on the database.'''
     @blp.response(200, TaskSchema(many=True))
     def get(self):
         return TaskModel.query.filter_by(completed=False)
@@ -28,6 +30,8 @@ class ListUncompletedTasks(MethodView):
 class ListCompletedTasks(MethodView):
     @blp.response(200, TaskSchema(many=True))
     def get(self):
+        '''This Endpoint is used for show all completed tasks listed on the database.'''
+        
         return TaskModel.query.filter_by(completed=True)
     
 
@@ -35,6 +39,9 @@ class ListCompletedTasks(MethodView):
 class ListSingleTaks(MethodView):
     @blp.response(200, TaskSchema)
     def get(self, task_id):
+        '''This Endpoint is used for show a specific task listed on the database.
+        The task will be searched by its id.
+        '''
         task = TaskModel.query.get_or_404(task_id)
         return task
 
@@ -47,6 +54,9 @@ class CreateNewTask(MethodView):
     @blp.arguments(TaskSchema)
     @blp.response(201, TaskSchema)
     def post(self, task_data):
+        '''This Endpoint is used for create a new task on the database.
+        The task will be created with the given parameters.
+        '''
         create_time = datetime.now()
         when_created = create_time.strftime("%d/%m/%Y %H:%M:%S")
         task = TaskModel(**task_data, when_created=when_created)
@@ -58,6 +68,9 @@ class CreateNewTask(MethodView):
 class DeleteTask(MethodView):
     @blp.response(200, TaskSchema)
     def delete(self, task_id):
+        '''This Endpoint is used for delete a specific task from the database.
+        The task will be searched by its id.
+        '''
         task = TaskModel.query.get_or_404(task_id)
         db.session.delete(task)
         db.session.commit()
@@ -71,6 +84,10 @@ class UpdateTask(MethodView):
     @blp.response(200, UpdateTaskSchema)
     def put(self, inc_data, task_id):
         
+        '''This Endpoint is used for update a specific task on the database.
+        The task will be searched by its id.
+        The task will be updated with the given parameters.
+        '''
         task = TaskModel.query.get(task_id)
         try:
             task.title = inc_data["title"]
@@ -89,6 +106,10 @@ class UpdateTask(MethodView):
 class CompleteATask(MethodView):
     @blp.response(200, TaskSchema)
     def put(self, task_id):
+        '''This Endpoint is used for mark a specific task as completed on the database.
+        The task will be searched by its id.
+        The task will be updated with the given parameters.
+        '''
         task = TaskModel.query.get(task_id)
         task.completed = True
         task.when_completed = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
